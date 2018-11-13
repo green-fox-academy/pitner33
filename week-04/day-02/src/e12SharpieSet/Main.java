c0f62e6bc30da883bf17fcd2f9657a94c7e8eda2
Peter Kovacs <pitner33@gmail.com>
2018-11-13 17:47:09 +0100

    (add) w04d02 end of the day

---
+++ week-04/day-02/src/e12SharpieSet/Main.java
@@ -0,0 +1,15 @@
+package e12SharpieSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Sharpie nr1 = new Sharpie("Green", 2f);
+        Sharpie nr2 = new Sharpie("Gold", 4f);
+        Sharpie nr3 = new Sharpie("Orange", 6f);
+        Sharpie nr4 = new Sharpie("Black", 3f);
+        Sharpie nr5 = new Sharpie("Red", 5f);
+
+        SharpieSet sharpieList = new SharpieSet();
+
+
+    }
+}
---
+++ week-04/day-02/src/e12SharpieSet/SharpieSet.java
@@ -0,0 +1,20 @@
+package e12SharpieSet;
+
+import java.util.List;
+
+/*Reuse your Sharpie class
+Create SharpieSet class
+        it contains a list of Sharpie
+        countUsable() -> sharpie is usable if it has ink in it
+        removeTrash() -> removes all unusable sharpies*/
+public class SharpieSet {
+    List<String> sharpieList;
+
+    public void countUsable() {
+
+    }
+
+    public void removeTrash() {
+
+    }
+}
