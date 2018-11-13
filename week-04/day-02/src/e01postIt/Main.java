bd79c8cbd32e910568512b51b0ee83be06c68e2f
Peter Kovacs <pitner33@gmail.com>
2018-11-13 11:19:53 +0100

    (add) w04d02 e01postIt

--- week-04/day-02/src/e01postIt/Main.java
+++ week-04/day-02/src/e01postIt/Main.java
@@ -1,4 +1,20 @@
 package e01postIt;
 
 public class Main {
+    public static void main(String[] args) {
+        PostIt p1 = new PostIt();
+        p1.color = "Blue";
+        p1.backgrounColor = "Orange";
+        p1.text = "Idea 1";
+
+        PostIt p2 = new PostIt();
+        p2.color = "Black";
+        p2.backgrounColor = "Pink";
+        p2.text = "Awsome";
+
+        PostIt p3 = new PostIt();
+        p3.color = "Green";
+        p3.backgrounColor = "Yellow";
+        p3.text = "Superb";
+    }
 }
--- week-04/day-02/src/e01postIt/PostIt.java
+++ week-04/day-02/src/e01postIt/PostIt.java
@@ -1,4 +1,15 @@
 package e01postIt;
-
+/*Create a PostIt class that has
+        a backgroundColor
+        a text on it
+        a textColor
+        Create a few example post-it objects:
+        an orange with blue text: "Idea 1"
+        a pink with black text: "Awesome"
+        a yellow with green text: "Superb!"*/
 public class PostIt {
+    String backgrounColor;
+    String text;
+    String color;
+
 }
