c6ebfaf15b81f4d8eb0b5ffc034f74edbfc2343f
Peter Kovacs <pitner33@gmail.com>
2018-11-14 17:41:04 +0100

    (fix) notes for w04d01 e08Lottery

--- week-04/day-01/src/e08lottery.java
+++ week-04/day-01/src/e08lottery.java
@@ -33,5 +33,9 @@ public class e08lottery {
 
 
     }
-
+//    az 5 leggyakoribbhoz. dupla fori ami ha egyezést talál count++, ezután az i-diket beteszi egy új tömbbe, a count-al együtt
+//    ?? 2 dimenziós tömb? mérete az eredeti lista size ill 2
+//    az i ugik egyet, count nulláz, ha egyezést talál count++ és az új listába legelőre teszi, ha a countja nagyobb, mint az aktuális első ott
+//    így mindig a legnagyobb count-ú lesz elöl, [szám][count] formában
+//    ezután csak ki kell nyomtatni a multitömb első 5 elemét
 }
