2782197083db43e0ab2a3741560caab2c018391d
Peter Kovacs <pitner33@gmail.com>
2019-01-15 15:44:52 +0100

    (add) pw0d2 todo with authentication

---
+++ PROJECT PHASE/week01-authentication-todo/src/main/java/com/greenfoxacademy/authenticatedtodo/User/ApplicationUserRepository.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.authenticatedtodo.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
